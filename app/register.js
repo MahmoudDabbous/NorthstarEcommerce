956e18c14c30f81572464c85d53729e6c03b7d33
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-03-16 22:38:48 +0200

    stach

---
+++ app/register.js
@@ -0,0 +1,20 @@
+import Authenticator from "../src/authenticator.js";
+
+const form = document.querySelector("#app #register form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = form.email.value;
+    const password = form.password.value;
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const name = `${firstName} ${lastName}`;
+    const result = Authenticator.register(email, { password, name, cart: [], wishlist: [] });
+    if (result) {
+        window.location.href = "/ITI-Javascrit-Project/login";
+    } else {
+        const error = document.getElementById("form-error");
+        error.innerHTML = "User already exists";
+        error.classList.remove("d-none");
+    }
+});
